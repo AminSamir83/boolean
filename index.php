b06c7e13c0a98983d0e5a0cccf02a1c9ba7ceadd
AminSamir83 <37070162+AminSamir83@users.noreply.github.com>
2018-11-08 11:57:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 08/11/2018
+ * Time: 11:53
+ */
+$a="0";
+$b="TRUE";
+$c=FALSE;
+$d=($a OR $b);
+$e=($a AND $c);
+$f=($a XOR $b);
+echo "FALSE TRUE FALSE TRUE FALSE TRUE"
+?>
